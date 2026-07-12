518fb0c80012ee9d69c4b836c190f635cdd22bf9
Astryn25 <rishipalod2404@gmail.com>
2026-07-12 07:19:26 +0530

    Update default port to 2431

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     max_memory_restart: '512M',
     env_production: {
       NODE_ENV: 'production',
-      PORT:     3000,
+      PORT:     2431,
     },
     error_file:      'logs/error.log',
     out_file:        'logs/out.log',
